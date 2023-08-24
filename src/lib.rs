79649da68b6b491461a40bf59ca9ec261fc39fd1
Dawson <30784509+funkemunky@users.noreply.github.com>
2023-08-24 11:30:51 -0400

    Adding bulma extensions files

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod layout;
 
 pub mod prelude {
     pub use crate::{columns::*, components::*, elements::*, layout::*};
-    pub use crate::{get_bulma_cdn, get_bulma_css, Colors, Sizes};
+    pub use crate::{get_bulma_cdn, get_bulma_css, get_bulma_ext_cdn, get_bulma_extjs_cdn, Colors, Sizes};
 }
 
 /// include bulma with css content
@@ -18,6 +18,14 @@ pub fn get_bulma_css() -> &'static str {
 /// get bulma cdn url
 pub fn get_bulma_cdn() -> &'static str {
     "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+}
+
+pub fn get_bulma_ext_cdn() -> &'static str {
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/dist/css/bulma-extensions.min.css"
+}
+
+pub fn get_bulma_extjs_cdn() -> &'static str {
+    "https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/dist/js/bulma-extensions.min.js"
 }
 
 #[derive(Clone, PartialEq)]
